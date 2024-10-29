bce2cd8584f639881e2ecceac29bf9f6e653a9f5
binx <iconiclampard99@gmail.com>
2024-10-29 09:44:01 +0700

    fix switch mode with icon

--- script.js
+++ script.js
@@ -1,29 +1,62 @@
 // dark mode & light mode
+// document.addEventListener('DOMContentLoaded', function() {
+//     const toggleButton = document.getElementById('theme-switch');
+//     const body = document.body;
+
+//     const currentTheme = localStorage.getItem('theme');
+//     if(currentTheme) {
+//         body.classList.add(currentTheme);
+
+//         if(currentTheme === 'light-mode') {
+//             toggleButton.textContent = 'switch to dark mode';
+//         }
+//     }
+
+//     toggleButton.addEventListener('click', function() {
+//         body.classList.toggle('light-mode');
+
+//         if(body.classList.contains('light-mode')) {
+//             toggleButton.textContent = 'switch to dark mode';
+//             localStorage.setItem('theme', 'light-mode');
+//         } else {
+//             toggleButton.textContent = 'switch to light mode';
+//             localStorage.setItem('theme', 'dark-mode');
+//         }
+//     });
+// });
+
+// dark mode & light mode with icon
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('theme-switch');
+    const icon = toggleButton.querySelector('i');
     const body = document.body;
 
+    // set theme based on local storage
     const currentTheme = localStorage.getItem('theme');
     if(currentTheme) {
         body.classList.add(currentTheme);
 
         if(currentTheme === 'light-mode') {
-            toggleButton.textContent = 'switch to dark mode';
-        }
-    }
+            icon.classList.replace('fa-sun', 'fa-moon');
+        };
+    };
 
     toggleButton.addEventListener('click', function() {
         body.classList.toggle('light-mode');
 
         if(body.classList.contains('light-mode')) {
-            toggleButton.textContent = 'switch to dark mode';
+            icon.classList.replace('fa-sun', 'fa-moon');
             localStorage.setItem('theme', 'light-mode');
         } else {
-            toggleButton.textContent = 'switch to light mode';
+            icon.classList.replace('fa-moon', 'fa-sun');
             localStorage.setItem('theme', 'dark-mode');
-        }
+        };
     });
 });
+
+
+
+
 
 // scroll to top
 const scrollToTop = document.getElementById('scrollToTopId');
